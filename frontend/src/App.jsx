cd01290e73b5e39912d367a763c6513eb630b096
AikoFr <aikootv@gmail.com>
2023-04-13 09:39:58 +0200

    Ajout et Config de SASS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import Home from "./pages/Home";
 // US 1 import
 
 // US 2 Import
@@ -7,18 +6,18 @@ import Home from "./pages/Home";
 
 // US 4 import
 
-
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       {/** US 1 Page */}
+
       {/** US 2 Page */}
+
       {/** US 3 Page */}
+
       {/** US 4 Page */}
-      <p>coucou</p>
     </div>
   );
 }
