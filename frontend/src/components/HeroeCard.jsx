ece4dec9db9b77d371bdabca6468679303936ea0
Jumzer <bluevictor@hotmail.fr>
2023-04-19 16:31:25 +0200

    improving cardHeroe

--- frontend/src/components/HeroeCard.jsx
+++ frontend/src/components/HeroeCard.jsx
@@ -6,13 +6,39 @@ function HeroeCard({ Heroes }) {
       <div className="imgSection">
         <img src={Heroes[0].image_md} alt="" />
       </div>
-      <section className="specSection">
-        <h2 className="specElement">{Heroes[0].name}</h2>
-        <h3 className="specElement"> Description </h3>
-        <p className="specElement">{Heroes[0].gender}</p>
-        <p className="specElement">{Heroes[0].race}</p>
-        <p className="specElement">Eye color : {Heroes[0].eyeColor}</p>
-        <p className="specElement">Hair color : {Heroes[0].hairColor}</p>
+      <section className="infoSection">
+        <section className="descSection">
+          <h2 className="descElement">{Heroes[0].name}</h2>
+          <h3 className="descElement">
+            {Heroes[0].gender} - {Heroes[0].race}
+          </h3>
+          <p className="descElement">
+            {Heroes[0].fullName} | Born in {Heroes[0].placeOfBirth}
+          </p>
+          <h3 className="descElement">Caracteristics</h3>
+          <p className="descElement">
+            {Heroes[0].eyeColor} eyes & {Heroes[0].hairColor}
+          </p>
+          <p className="descElement">
+            First appeared in {Heroes[0].firstAppearance}
+          </p>
+          <p className="descElement">Work as {Heroes[0].work}</p>
+        </section>
+        <section className="statSection">
+          <h3>Statistics</h3>
+
+          {/* <ul>
+            <li>Intelligence : {Heroes[0].intelligence} </li>
+            <li>Strength : {Heroes[0].strength}</li>
+            <li>Speed : {Heroes[0].speed}</li>
+            <li>Durability : {Heroes[0].durability}</li>
+            <li>Power : {Heroes[0].power}</li>
+            <li>Combat : {Heroes[0].combat}</li>
+          </ul> */}
+        </section>
+        <section className="buttonSection">
+          <button type="button">Louer mon super héros</button>
+        </section>
       </section>
     </div>
   );
