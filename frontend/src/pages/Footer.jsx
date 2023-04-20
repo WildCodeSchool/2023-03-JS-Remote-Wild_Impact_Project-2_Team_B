7054b0b80d808357e8b0b9821bf2c7ba1ecb4d29
Laure Graves <graveslaure@gmail.com>
2023-04-20 21:04:40 +0200

    modifs txt colonnes modif bouton fonct partiellement ajout media query

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -1,80 +1,37 @@
 import React from "react";
 
-// function Footer() {
-//   return (
-//     // <div className="main-footer">
-//       <div className="container">
-//         <div className="row">
-//           {/* Column1 */}
-//           <div className="col1">
-//             <h4>Narvel</h4>
-//             <h1 className="list-unstyled">
-//               <li>Location de supers héros</li>
-//               <p>Blablabla bla bla bla bla</p>
-//             </h1>
-//           </div>
-//           {/* Column2 */}
-//           <div className="col2">
-//             <h4>Menu</h4>
-//             <ul className="list-unstyled">
-//               <li>Home</li>
-//               <li>Catalogue</li>
-//               <li>Cards</li>
-//             </ul>
-//           </div>
-//           {/* Column3 */}
-//           <div className="col3">
-//             <h4>Contact</h4>
-//             <ul className="list-unstyled">
-//               <li>1 </li>
-//               <li>2</li>
-//               <li>3</li>
-//             </ul>
-//           </div>
-//         </div>
-//         <hr />
-//         <div className="row1">
-//           <p className="col4">
-//             &copy;{new Date().getFullYear()} | Narvel France |
-//             All rights reserved
-//           </p>
-//         </div>
-//       </div>
-//   );
-// }
-
-// export default Footer;
-
 function Footer() {
   return (
     <footer className="footer-container">
       <div className="row1">
         <div className="footer-column">
+        <logo className= "footer-logo" type="logo"></logo>
           <h3>Narvel Logo</h3>
           <h4 className="list-unstyled">
-            <p>Location de supers héros</p>
-            <p>Blablabla bla bla bla bla</p>
+            <p>Siège social </p>
+            <p>500 S. Buena Vista Street, Californie États-Unis</p>
           </h4>
         </div>
 
         <div className="footer-column1">
-          <h3 className="footer-title">Menu</h3>
+          {/* <h3 className="footer-title">Menu</h3> */}
           <ul className="list-unstyled">
-            <li>Home</li>
-            <li>Catalogue</li>
-            <li>Cards</li>
+            <li className="footer-item" >Qui sommes nous ?</li>
+            <li className="footer-item">Recrutement</li>
+            <li className="footer-item">Presse et actualité</li>
           </ul>
         </div>
 
         <div className="footer-column2">
-          <h4>Contactez nous</h4>
-          <ul className="list-unstyled" />
+        <h3 className="footer-title">Besoin d'aide</h3>
+          <button className= "footer-button" type="button">Contactez nous</button>
+          {/* <h4>Contactez nous</h4> */}
         </div>
       </div>
 
       <div className="row2">
         <p className="col4">
-          &copy;{new Date().getFullYear()} | Narvel France | All rights reserved
+          &copy;{new Date().getFullYear()} | Narvel France | All rights reserved | CGV | CGU | Mentions légales | Gestion des cookies
         </p>
       </div>
     </footer>
