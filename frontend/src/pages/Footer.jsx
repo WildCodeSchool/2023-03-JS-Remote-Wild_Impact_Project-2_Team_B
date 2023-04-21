ed97cc093690a55cf56090f2af949cbc65513d7e
Laure Graves <graveslaure@gmail.com>
2023-04-21 09:37:50 +0200

    derniere ligne footer centrée

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="footer-container">
       <div className="row1">
         <div className="footer-column">
-        <logo className= "footer-logo" type="logo"></logo>
+          <logo className="footer-logo" type="logo" />
           <h3>Narvel Logo</h3>
           <h4 className="list-unstyled">
             <p>Siège social </p>
@@ -16,22 +16,25 @@ function Footer() {
         <div className="footer-column1">
           {/* <h3 className="footer-title">Menu</h3> */}
           <ul className="list-unstyled">
-            <li className="footer-item" >Qui sommes nous ?</li>
+            <li className="footer-item">Qui sommes nous ?</li>
             <li className="footer-item">Recrutement</li>
             <li className="footer-item">Presse et actualité</li>
           </ul>
         </div>
 
         <div className="footer-column2">
-        <h3 className="footer-title">Besoin d'aide</h3>
-          <button className= "footer-button" type="button">Contactez nous</button>
+          <h3 className="footer-title">Besoin d'aide</h3>
+          <button className="footer-button" type="button">
+            Contactez nous
+          </button>
           {/* <h4>Contactez nous</h4> */}
         </div>
       </div>
 
       <div className="row2">
         <p className="col4">
-          &copy;{new Date().getFullYear()} | Narvel France | All rights reserved | CGV | CGU | Mentions légales | Gestion des cookies
+          &copy;{new Date().getFullYear()} | Narvel France | All rights reserved
+          | CGV | CGU | Mentions légales | Gestion des cookies
         </p>
       </div>
     </footer>
