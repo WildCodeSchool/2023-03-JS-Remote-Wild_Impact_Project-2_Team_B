2f3ab2e74137b34fe0f378a095cc44b3ea2704dd
Jumzer <bluevictor@hotmail.fr>
2023-04-26 14:10:45 +0200

    creating routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+// const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+
+const connection = require("./database");
+
+connection
+  .getConnection()
+  .then(() => {
+    console.info("Can reach database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 module.exports = router;
