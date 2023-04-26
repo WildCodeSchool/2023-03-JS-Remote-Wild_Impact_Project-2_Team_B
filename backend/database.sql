600e05a34643723c082173e03370d9633d71e073
Jumzer <bluevictor@hotmail.fr>
2023-04-26 10:39:32 +0200

    insering first heroes

--- backend/database.sql
+++ backend/database.sql
@@ -21,3 +21,29 @@ CREATE TABLE `heroe` (
 PRIMARY KEY (`id`)
 );
 
+INSERT INTO `heroe` (name, slug, image_sm, image_md, gender,race, eyeColor, hairColor, fullName, placeOfBirth, firstAppearance,work, intelligence, strength, speed, durability, power, combat )
+VALUES ('A-Bomb', '1-a-bomb', 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/1-a-bomb.jpg', 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/1-a-bomb.jpg', 'Male', 'Human', 'Yellow', 'No Hair' , 'Richard Milhouse Jones', 'Scarsdale, Arizona', 'Hulk Vol 2 #2 (April, 2008) (as A-Bomb)', 'Musician, adventurer, author; formerly talk show host', 38, 100, 17, 80, 24, 64),
+("Abe Sapien", "2-abe-sapien","https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/2-abe-sapien.jpg", 
+"https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/2-abe-sapien.jpg",
+"Male", "Icthyo Sapien","Blue","No Hair","Abraham Sapien","-","Hellboy: Seed of Destruction (1993)","Paranormal Investigator",88, 28, 35, 65, 100, 85),
+(
+ 'Abin Sur',
+ '3-abin-sur',
+ 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/3-abin-sur.jpg',
+ 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/3-abin-sur.jpg',
+ 'Male',
+ 'Ungaran',
+ 'Blue',
+  'No Hair',
+  '',
+   'Ungara',
+   'Showcase #22 (October, 1959)',
+   'Green Lantern, former history professor',
+   50,
+   90,
+   53,
+   64,
+   99,
+   65
+  ),
+  ('Abomination', '4-abomination', 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/4-abomination.jpg', 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/4-abomination.jpg', 'Male', 'Human / Radiation', 'Green', 'No Hair', 'Emil Blonsky', 'Zagreb, Yugoslavia', 'Tales to Astonish #90', 'Ex-Spy', 63, 80, 53, 90, 62, 95);
