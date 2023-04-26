9a06e9bfb7c748ac629d0317b57d525ae9dbf8fa
Jumzer <bluevictor@hotmail.fr>
2023-04-26 10:09:30 +0200

    creating database and table

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,23 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `heroe` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(100) NOT NULL,
+`slug` VARCHAR(100),
+`image_sm` VARCHAR(255) NOT NULL,
+`image_md` VARCHAR(255) NOT NULL,
+`gender` VARCHAR(100) NOT NULL,
+`race` VARCHAR(100),
+`eyeColor` VARCHAR(100) NOT NULL,
+`hairColor` VARCHAR(100) NOT NULL,
+`fullName` VARCHAR(100),
+`placeOfBirth` VARCHAR(100),
+`firstAppearance` VARCHAR(200) NOT NULL,
+`work` VARCHAR(200) NOT NULL,
+`intelligence` INT,
+`strength` INT,
+`speed` INT,
+`durability` INT,
+`power` INT,
+`combat` INT,
+PRIMARY KEY (`id`)
+);
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
