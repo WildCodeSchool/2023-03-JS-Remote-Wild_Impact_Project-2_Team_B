d14855eb40ecdf3eea4862b37332b248766cc85a
Jumzer <bluevictor@hotmail.fr>
2023-04-27 15:53:39 +0200

    routes with params

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,42 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+const connection = require("./database");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+connection
+  .getConnection()
+  .then(() => {
+    console.info("Can reach database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+router.get("/", (req, res) => {
+  res.status(200).send("Ceci est la HomePage");
+});
+
+router.get("/heroes", (req, res) => {
+  connection
+    .query("SELECT * FROM heroe")
+    .then(([result]) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+router.get("/heroes/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  connection
+    .query(`SELECT * FROM heroe WHERE id = ${id}`)
+    .then(([result]) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 module.exports = router;
